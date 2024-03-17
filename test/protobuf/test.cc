a01f4823cd4bab0fe2b802046e2009f6475eeb71
changjiekuAka <823758408@qq.com>
2024-03-17 16:02:58 +0800

    test

--- test/protobuf/test.cc
+++ test/protobuf/test.cc
@@ -5,15 +5,25 @@ using namespace fixbug;
 
 int main()
 {
-    LoginRequest req;
-    req.set_name("zhang san");
-    req.set_pwd("12345");
+    // LoginRequest req;
+    // req.set_name("zhang san");
+    // req.set_pwd("12345");
 
-    std::string str;
-    if(req.SerializeToString(&str)){
-        std::cout << str << std::endl;
-    }
+    // std::string str;
+    // if(req.SerializeToString(&str)){
+    //     std::cout << str << std::endl;
+    // }
 
+    GetFriendListsResponse rsp;
+    ResultCode *rc = rsp.mutable_result();
+    rc->set_errcode(1);
+
+    user* user2 = rsp.add_friend_list();
+    user2->set_age(20);
+    user2->set_name("zhang san");
+    user2->set_sex(user::MAN);
+
+    std::cout << rsp.friend_list_size() << std::endl;
 
     return 0;
 }
