c451d8d0a49af0a49edb59c5d18781edfdd9b370
changjiekuAka <823758408@qq.com>
2024-03-20 14:16:54 +0800

    app

--- src/include/mprpcapplication.h
+++ src/include/mprpcapplication.h
@@ -1,1 +1,18 @@
 #pragma once
+
+class mprpcapplication
+{
+private:
+    mprpcapplication(const mprpcapplication&) = delete;
+    mprpcapplication(mprpcapplication&&) = delete;
+    mprpcapplication() {}
+public:
+    mprpcapplication(int argc,char** argv);
+    static mprpcapplication& GetInstance()
+    {
+        static mprpcapplication ins;
+        return ins;
+    }
+};
+
+
