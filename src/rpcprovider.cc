8b5c4e28b49df56b87e4a409406259a55a59f494
changjiekuAka <823758408@qq.com>
2024-03-29 16:22:42 +0800

    muduo

--- src/rpcprovider.cc
+++ src/rpcprovider.cc
@@ -11,18 +11,21 @@ void RpcProvider::NotifyService(::google::protobuf::Service* service)
 
 void RpcProvider::Run()
 {
-    std::string ip = mprpcapplication::GetInstance().GetConfig().Load("rpcserverip");
-    uint16_t port = std::stoi( mprpcapplication::GetInstance().GetConfig().Load("rpcserverport").c_str());
+    std::string ip = mprpcapplication::GetInstance().GetConfig().Load("rpcservicesIP");
+    uint16_t port = std::atoi( mprpcapplication::GetInstance().GetConfig().Load("rpcservicesPort").c_str());
     muduo::net::InetAddress inetaddress(ip,port);
     
     // 组合TcpServer对象
-    muduo::net::TcpServer m_tcpserver(&_eventloop,inetaddress,"rpcprovider");
+    muduo::net::TcpServer tcpserver(&_eventloop,inetaddress,"rpcprovider");
     // 注册用户连接创捷回调
-    m_tcpserver.setConnectionCallback(std::bind(&RpcProvider::OnConnection,this,_1));
+    tcpserver.setConnectionCallback(std::bind(&RpcProvider::OnConnection,this,_1));
     // 注册用户用户读写事件回调
-    m_tcpserver.setMessageCallback(std::bind(&RpcProvider::OnMessage,this,_1,_2,_3));
+    tcpserver.setMessageCallback(std::bind(&RpcProvider::OnMessage,this,_1,_2,_3));
 
-    m_tcpserver.setThreadNum(4);
+    tcpserver.setThreadNum(4);
+
+    tcpserver.start();
+    _eventloop.loop();
 }
 
 void RpcProvider::OnConnection(const muduo::net::TcpConnectionPtr& conn)
